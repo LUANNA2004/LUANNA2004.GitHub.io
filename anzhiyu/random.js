2218b126cbceb44d7409d53028bd37043ab75f0d
LUNNA2004 <3043796420@qq.com>
2025-04-07 14:48:46 +0800

    Site updated: 2025-04-07 14:48:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/06/hello-world/"];function toRandomPost(){
+var posts=["2025/04/06/hello-world/","2025/04/07/我的第一篇博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
